9eb0905e184e675f69919717117be34ba1f3d350
Black Market Crash Dummy <foodcoa@gmail.com>
2026-02-18 10:40:16 -0500

    Add phased P2P data-plane scaffolding behind labs feature

---
+++ src/p2p/index.ts
@@ -0,0 +1,10 @@
+/*
+Copyright 2024 New Vector Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE files in the repository root for full details.
+*/
+
+export { PeerManager, type PeerSnapshot } from "./peerManager";
+export { RoomMesh, type ChunkInventory } from "./roomMesh";
+export { RTCTransport, type RTCEnvelope, type RTCPeerHandle } from "./rtcTransport";
